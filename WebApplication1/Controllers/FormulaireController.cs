53f4ef3d1c7a094572715b3fb6cbf8074ed36e5a
François Lépine <lepine.francois@gatineau.ca>
2018-05-17 22:40:53 -0400

    Modifications pour l'exercice.

---
+++ WebApplication1/Controllers/FormulaireController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FormulaireController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Procède à l'inscription suite au clic du bouton 'Envoyer' du formulaire.
+        /// </summary>
+        /// <param name="donneesFormulaire">Données provenant du formulaire.</param>
+        /// <returns>Résultat d'action (Code HTTP).</returns>
+        public ActionResult Inscrire(AjouterEmploye donneesFormulaire)
+        {
+            throw new NotImplementedException();
+
+            // Voici les étapes à faire.
+            // 1. Vérifier les données.
+            // 1.1 Nom vide => lancer une exception.
+            // 1.2 Prénom vide => lancer une exception.
+            // 1.3 Email vide => lancer une exception.
+
+            // 2. Écrire dans la console pour y voir les informations.
+
+            // 3. Retourner à l'écran principal et y dire un beau bonjour!
+        }
+    }
+}
---
+++ WebApplication1/Models/AjouterEmploye.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AjouterEmploye
+    {
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Email { get; set; }
+    }
+}
